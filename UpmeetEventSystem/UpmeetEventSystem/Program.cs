31d94156454b0f73cc0ce5e7bb7f177a9001ba89
Brittany Blackmon <brittanylynnwright@gmail.com>
2023-02-23 22:48:46 -0500

    Almost there

--- UpmeetEventSystem/UpmeetEventSystem/Program.cs
+++ UpmeetEventSystem/UpmeetEventSystem/Program.cs
@@ -2,7 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: "CorsPolicy",
+      builder =>
+      {
+        builder.SetIsOriginAllowed(origin => true);
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+        builder.AllowCredentials();
+      });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +30,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
