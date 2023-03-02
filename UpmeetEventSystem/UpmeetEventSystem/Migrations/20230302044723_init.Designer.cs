fd86ef8cb3b2d531eafd4a892acc81ae375b7384
colin <cjsteward07@gmail.com>
2023-03-02 00:01:12 -0500

    update migrations

--- UpmeetEventSystem/UpmeetEventSystem/Migrations/20230302044723_init.Designer.cs
+++ UpmeetEventSystem/UpmeetEventSystem/Migrations/20230302044723_init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UpmeetEventSystem.DAL;
 
@@ -11,9 +12,10 @@ using UpmeetEventSystem.DAL;
 namespace UpmeetEventSystem.Migrations
 {
     [DbContext(typeof(EventContext))]
-    partial class EventContextModelSnapshot : ModelSnapshot
+    [Migration("20230302044723_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
